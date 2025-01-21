a47bcfd34bbf766d1fe9d5dae312c526ca45e635
Jay Park <wngt3319@gmail.com>
2025-01-21 11:55:41 +0900

    🔥 remove elysiajs/html

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,8 @@
-import html from "@elysiajs/html";
-import staticPlugin from "@elysiajs/static";
-import { Elysia } from "elysia";
+import staticPlugin from '@elysiajs/static'
+import { file } from 'bun'
+import { Elysia } from 'elysia'
 
 new Elysia()
-  .use(staticPlugin({ assets: "dist" }))
-  .use(html())
-  .get(
-    "/",
-    () => `<!DOCTYPE html>
-      <html lang="en">
-        <head>
-          <meta charset="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>React App</title>
-        </head>
-        <body>
-          <div id="root"></div>
-          <script src="/public/page.js"></script>
-        </body>
-      </html>
-    `
-  )
-  .listen(3000);
+  .use(staticPlugin({ assets: 'dist' }))
+  .get('/', () => file('./index.html'))
+  .listen(3000)
